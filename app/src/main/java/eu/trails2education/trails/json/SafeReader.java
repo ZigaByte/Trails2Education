6c261cf2f1263db10254894d9922c97bb69b2352
ZigaByte <zvene28@gmail.com>
2017-12-19 19:29:01 +0100

    Created a SafeReader class for JSON reading with default values if the JSON does not contain the data. Not yet tested

---
+++ app/src/main/java/eu/trails2education/trails/json/SafeReader.java
@@ -0,0 +1,57 @@
+package eu.trails2education.trails.json;
+
+import android.util.Log;
+
+import org.json.JSONObject;
+
+/**
+ * Created by Ziga on 19-Dec-17.
+ */
+
+public class SafeReader {
+
+    /**
+     * Safely reads the integer at @key in the provided object. If it does not exist or is null
+     * it returns the provided default value.
+     * */
+    public static int readInt(JSONObject object, String key, int defaultValue) {
+        if(!object.has(key) || object.isNull(key))
+            return defaultValue;
+        try{
+            int value = object.getInt(key);
+            return value;
+        }catch (Exception e){
+            Log.e("JSON int EXCEPTION", "A JSON int READING EXCETPTION");
+            return defaultValue;
+        }
+    }
+
+    /** Safe integer read with default value 0. */
+    public static int readInt(JSONObject object, String key){
+        return readInt(object, key, 0);
+    }
+
+    /**
+     * Safely reads the String at @key in the provided object. If it does not exist or is null
+     * it returns the provided default value.
+     * */
+    public static String readString(JSONObject object, String key, String defaultValue) {
+        if(!object.has(key) || object.isNull(key))
+            return defaultValue;
+        try{
+            String value = object.getString(key);
+            return value;
+        }catch (Exception e){
+            Log.e("JSON String EXCEPTION", "A JSON String READING EXCETPTION");
+            return defaultValue;
+        }
+    }
+
+    /** Safe String read with default value of an empty string. */
+    public static String readString(JSONObject object, String key){
+        return readString(object, key, "");
+    }
+
+
+
+}
